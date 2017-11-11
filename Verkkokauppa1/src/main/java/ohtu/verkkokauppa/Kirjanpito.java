5b0dc97764172c275a20b549b4d482e18ae5a998
wileht <wille.lehtomaki@helsinki.fi>
2017-11-11 15:10:30 +0200

    Verkkokauppa

---
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -0,0 +1,38 @@
+
+package ohtu.verkkokauppa;
+
+import java.util.ArrayList;
+
+public class Kirjanpito implements KirjanpitoIF {
+//    private static KirjanpitoIF instance;
+//    
+//    public static KirjanpitoIF getInstance() {
+//        if ( instance==null) {
+//            instance = new Kirjanpito();
+//        }
+//        
+//        return instance;
+//    }
+    
+    private ArrayList<String> tapahtumat;
+
+    public Kirjanpito() {
+        tapahtumat = new ArrayList<String>();
+    }
+    
+    /* (non-Javadoc)
+	 * @see ohtu.verkkokauppa.KirjanpitoIF#lisaaTapahtuma(java.lang.String)
+	 */
+    @Override
+	public void lisaaTapahtuma(String tapahtuma) {
+        tapahtumat.add(tapahtuma);
+    }
+
+    /* (non-Javadoc)
+	 * @see ohtu.verkkokauppa.KirjanpitoIF#getTapahtumat()
+	 */
+    @Override
+	public ArrayList<String> getTapahtumat() {
+        return tapahtumat;
+    }       
+}
